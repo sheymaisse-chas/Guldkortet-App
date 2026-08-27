171f5ef97c66f35eccecfc0c9f6151e6c214c322
sheymaisse-chas <sheyma.isse@chasacademy.se>
2026-08-27 15:02:45 +0200

    Skapat gränssnittet och logiken för den

--- Guldkortet/Form1.Designer.cs
+++ Guldkortet/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lstRewards = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 62);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(228, 76);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Starta Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(12, 548);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(228, 76);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stoppa Server";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(1016, 9);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(194, 32);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status: Offline";
+            // 
+            // lstRewards
+            // 
+            this.lstRewards.FormattingEnabled = true;
+            this.lstRewards.ItemHeight = 31;
+            this.lstRewards.Location = new System.Drawing.Point(263, 62);
+            this.lstRewards.Name = "lstRewards";
+            this.lstRewards.Size = new System.Drawing.Size(947, 562);
+            this.lstRewards.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1231, 648);
+            this.Controls.Add(this.lstRewards);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ListBox lstRewards;
     }
 }
 
--- Guldkortet/Form1.cs
+++ Guldkortet/Form1.cs
@@ -1,20 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Guldkortet
 {
     public partial class Form1 : Form
     {
+        // 1. Skapar en generisk lista för belöningar
+        private List<Reward> rewardList = new List<Reward>();
+
+        // 2. Detta är en referens till nätverkshanteraren som sköter nätverkskommunikationen (TCP)
+        private ServerManager serverManager;
         public Form1()
         {
             InitializeComponent();
+
+            // Skapar ServerManager och skicka med detta formulär
+            serverManager = new ServerManager(this);
+        }
+
+        /* --- KNAPPAR & HANDLER-METODER --- */
+
+        // Startar servern när användaren klickar på Starta-knappen.
+        // Ändrar lblStatus så att den visar att servern är kopplad.
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            serverManager.StartServer();
+            lblStatus.Text = "Status: Online (Lyssnar på port 12345)";
+            lblStatus.ForeColor = System.Drawing.Color.Green;
+        }
+
+        // Stoppa servern när användaren klickar på Stoppa-knappen
+        // Ändrar lblStatus så att den visar att servern inte är kopplad.
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            serverManager.StopServer();
+            lblStatus.Text = "Status: Offline";
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+        }
+
+        /* --- METOD SOM ANROPAS FRÅN SERVERMANAGER --- */
+
+        // Tar emot ett godkänt belöningsobjekt från ServerManager,
+        // sparar det i listan, uppdaterar gränssnittet och loggar till fil.
+        public void AddRewardToList(Reward reward, string userId)
+        {
+            // Eftersom TCP körs i bakgrunden använder vi Invoke för att uppdatera UI säkert
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => AddRewardToList(reward, userId)));
+                return;
+            }
+
+            // 1. Sparar list-objektet i minnet
+            rewardList.Add(reward);
+
+            // 2. Skapar presentationstext via polymorfism
+            string displayInfo = $"[Kund: {userId}] - {reward.Name}: {reward.GenerateMessage()}";
+
+            // 3. Visar i gränssnittets ListBox
+            lstRewards.Items.Add(displayInfo);
+
+            // 4. Loggar händelsen till log.txt
+            LogToFile(userId, reward);
+        }
+
+        /* --- FILHANTERING (log.txt enligt avsnitt 5.2) --- */
+
+        // Loggar godkända vinster med tidsstämpel till log.txt
+        private void LogToFile(string userId, Reward reward)
+        {
+            try
+            {
+                string logPath = "log.txt";
+                string timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                string logMessage = $"[{timeStamp}] Användare: {userId} löste in kort -> Belöning: {reward.Name}";
+
+                // Använder StreamWriter med append: true för att bevara tidigare data
+                using (StreamWriter writer = new StreamWriter(logPath, true, Encoding.UTF8))
+                {
+                    writer.WriteLine(logMessage);
+                }
+            }
+            // Om programmet inte kan logga eller avbryts i processen
+            catch (Exception ex)
+            {
+                MessageBox.Show("Kunde inte skriva till loggfilen: " + ex.Message);
+            }
+        }
+
+        // Stänger servern säkert om fönstret stängs
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serverManager.StopServer();
         }
     }
 }
